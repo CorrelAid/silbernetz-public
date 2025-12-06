06043cbd720a9f9e87ead8b709315ff0d3f9dd2d
Felix Kube <F.Kube@students.hertie-school.org>
2025-12-06 12:52:43 +0100

    branch for Win10 fix, executable trying

--- R/api.R
+++ R/api.R
@@ -126,7 +126,10 @@ download_numbers <- function(start_date, end_date) {
       )
     )) |>
     # type conversions for date and time
-    dplyr::mutate(date = lubridate::ymd(date), time = hms::as_hms(time))
+    dplyr::mutate(
+      date = lubridate::ymd(.data$date),
+      time = hms::as_hms(.data$time)
+  )
 }
 
 #' Perform a GET request to an API endpoint
