1ce28f68fcdb0c20b463f5488fff95be11f8beae
Felix Kube <53430714+flixi67@users.noreply.github.com>
2025-12-06 14:42:14 +0000

    Update app name and author in make-exec.R
    remove . which leads to errors

--- make-exec.R
+++ make-exec.R
@@ -1,10 +1,10 @@
   executablePackeR::pack(
-    app_name = "Silbernet e.V Reporting Tool",
+    app_name = "Silbernet Reporting Tool",
     option=list(is_dev=TRUE),
      electron_settings = list(
-       c("product_name_template", "Silbernet e.V Reporting Tool"),
+       c("product_name_template", "Silbernet Reporting Tool"),
        c("app_description_template", ""),
-       c("author_name_template", "CorrelAid e.V"),
+       c("author_name_template", "CorrelAid eV"),
        c("author_email_template", "leo.p@correlaid.org"),
        c("repository_url_template", "https://github.com/CorrelAid/silbernetz-public")
      )
