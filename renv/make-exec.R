d9f8fe09f2fd35f24ab6be168f1ac4e6d8040ece
Felix Kube <F.Kube@students.hertie-school.org>
2025-12-06 13:18:57 +0100

    add icons, add exec script

---
+++ renv/make-exec.R
@@ -0,0 +1,11 @@
+  executablePackeR::pack(
+    app_name = "Silbernet e.V Reporting Tool",
+    option=list(is_dev=TRUE),
+     electron_settings = list(
+       c("product_name_template", "Silbernet e.V Reporting Tool"),
+       c("app_description_template", ""),
+       c("author_name_template", "CorrelAid e.V"),
+       c("author_email_template", "leo.p@correlaid.org"),
+       c("repository_url_template", "https://github.com/CorrelAid/silbernetz-public")
+     )
+  )
