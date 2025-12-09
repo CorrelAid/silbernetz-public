29de8dd0a74981a151c5cee3a3fb0e4995962698
Leo <fr1e@pm.me>
2025-12-09 09:47:18 +0100

    try to log git hash

--- windows_deployment/update_code.bat
+++ windows_deployment/update_code.bat
@@ -7,20 +7,14 @@ git pull
 
 if %ERRORLEVEL% equ 0 (
     echo Code updated successfully!
-    :: Read the content of test.txt
-    set /p content=<test.txt
-    :: Remove any trailing whitespace or newline characters
-    set "content=%content:~0,-1%"
-    :: Define the new filename
-    set "newfilename=versions\%content%.txt"
-    :: Create the new file and write the content into it
-    echo %content% > "%newfilename%"
 
-    set 
     :: Write out current head commit id to current.txt
-    git rev-parse HEAD > current.txt
+    for /f %%i in ('git rev-parse HEAD') do set GITHASH=%%i
+    for /f %%i in ('powershell -NoProfile -Command "(Get-Date).ToString(\"yyyy-MM-dd\")"') do set ISODATE=%%i
+
+    echo %ISODATE%, %GITHASH% >> versions.txt
 ) else (
     echo Code did not update successfully!
 )
 
-::pause
+pause
